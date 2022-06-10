909de14917791280663c3c37a63d6d10f4540895
Thelolguy1 <18731514+Thelolguy1@users.noreply.github.com>
2022-06-09 17:44:12 -0700

    Edit install script

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak --user remote-add --no-gpg-verify grapejuiceTest GJTestRepo
 flatpak --user install grapejuiceTest com.gitlab.brinkervii.grapejuice
