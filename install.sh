18fd21b2e8a823ece7788c0f4d0fcf35b6c85544
Thelolguy1 <18731514+Thelolguy1@users.noreply.github.com>
2022-06-21 16:53:36 -0700

    Add runtime dep

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install org.freedesktop.Platform/x86_64/21.08
 flatpak --user remote-add --no-gpg-verify grapejuiceTest GJTestRepo
 flatpak --user install grapejuiceTest com.gitlab.brinkervii.grapejuice
