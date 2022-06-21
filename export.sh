65f59702b3d95359700081576c5923020c4b9914
lunarlattice <18731514+Thelolguy1@users.noreply.github.com>
2022-06-21 02:04:15 +0000

    Attempt to fix GL32

--- export.sh
+++ export.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-flatpak-builder --gpg-sign=B39B2B0F25BB843D5EB315B8A80327A9D7225985 --repo=GJTestRepo --force-clean build-dir com.gitlab.brinkervii.grapejuice.yml
+flatpak-builder --gpg-sign=628330EA01133645E2E4189C6CC7E205088B5BA4 --repo=GJTestRepo --force-clean build-dir com.gitlab.brinkervii.grapejuice.yml
