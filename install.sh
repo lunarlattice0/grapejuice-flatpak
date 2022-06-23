17f46f3aa353add75c8b2694f98b8add33481fc3
FazlyMR <69378798+FazlyMR@users.noreply.github.com>
2022-06-23 23:40:14 +0900

    Updated install.sh
    Adds the Flathub repository and installs Wine flatpak.

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install org.freedesktop.Platform/x86_64/21.08
+flatpak install app/org.winehq.Wine/x86_64/stable-21.08
 flatpak --user remote-add --no-gpg-verify grapejuiceTest GJTestRepo
 flatpak --user install grapejuiceTest com.gitlab.brinkervii.grapejuice
