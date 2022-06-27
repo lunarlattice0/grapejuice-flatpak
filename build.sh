5b3276310bebb1d7a045e16ad60c2fb9595e9d7e
Thelolguy1 <18731514+Thelolguy1@users.noreply.github.com>
2022-06-27 16:04:13 -0700

    Move some more tools

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-flatpak-builder build-dir/ com.gitlab.brinkervii.grapejuice.yml
-flatpak-builder --user --install --force-clean build-dir com.gitlab.brinkervii.grapejuice.yml
-echo "Done building"
--- export.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-flatpak-builder --gpg-sign=628330EA01133645E2E4189C6CC7E205088B5BA4 --repo=GJTestRepo --force-clean build-dir com.gitlab.brinkervii.grapejuice.yml
