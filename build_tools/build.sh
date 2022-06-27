29b4f5b4218b0dad38bd9d6900d0b0d4e7410424
lunarlattice <18731514+Thelolguy1@users.noreply.github.com>
2022-06-27 16:05:29 -0700

    Housekeeping (#21)
    * Move some tools around
    
    * Move some more tools

--- build_tools/build.sh
+++ build_tools/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd $PWD/..
 flatpak-builder build-dir/ com.gitlab.brinkervii.grapejuice.yml
 flatpak-builder --user --install --force-clean build-dir com.gitlab.brinkervii.grapejuice.yml
 echo "Done building"
