9bf5f35715ee1fa09fbf7b665e958dde3911cf48
Thelolguy1 <18731514+Thelolguy1@users.noreply.github.com>
2022-06-29 22:42:30 -0700

    shoehorn a fix into install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 flatpak remote-add --no-gpg-verify grapejuiceTest GJTestRepo
+flatpak install flathub org.freedesktop.Platform.Compat.i386 org.freedesktop.Platform.GL32.default
 flatpak install grapejuiceTest com.gitlab.brinkervii.grapejuice
